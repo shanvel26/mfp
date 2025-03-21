0e06a25ae3be8450dfbcdff57d4f641c6392705d
Shanmugavel Sundaramoorthy <shanvel26@gmail.com>
2025-03-22 02:36:10 +0530

    updated automated cache clear for AWS Cloudfront

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Container App</h1>
+      <h1>Container App, version 2</h1>
       <MarketingApp />
     </div>
   );
