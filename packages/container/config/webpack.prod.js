018e789089378feab8cdcd22ae91343fed592f69
Shanmugavel Sundaramoorthy <shanvel26@gmail.com>
2025-03-22 03:24:42 +0530

    added console log for debugging

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,6 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
+console.log('PRODUCTION_DOMAIN:', domain);
 
 const prodConfig = {
   mode: 'production',
